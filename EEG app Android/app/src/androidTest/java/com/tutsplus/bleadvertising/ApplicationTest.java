3af50e439b7966231e69e5a6e27d9829ec270e4d
Francesco Poluzzi <145541066+FrancescoPoluzzi@users.noreply.github.com>
2025-01-23 15:21:38 +0100

    Add Android app for simulating EEG peripheral

---
+++ EEG app Android/app/src/androidTest/java/com/tutsplus/bleadvertising/ApplicationTest.java
@@ -0,0 +1,24 @@
+package com.tutsplus.bleadvertising;
+
+import android.app.Application;
+import androidx.test.core.app.ApplicationProvider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertNotNull;
+//import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+/**
+ * Basic instrumentation test using AndroidJUnit4.
+ */
+/*
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest {
+
+    @Test
+    public void testApplicationNotNull() {
+        Application app = ApplicationProvider.getApplicationContext();
+        assertNotNull("Application is null", app);
+    }
+}*/
